10bea500faba752213b40b3b11dca915047426df
ronald <wesselingronald@gmail.com>
2021-12-03 22:10:55 +0100

    commit

--- openssh-server/unit_test.sh
+++ openssh-server/unit_test.sh
@@ -14,7 +14,7 @@ countdown() {
   echo
 }
 echo "Starten van openssh_test docker container voor test"
-docker run -d --name=openssh_test -e PUID=1000 -e PGID=1000 -e TZ=europe/amsterdam -e PUBLIC_KEY='ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCsIBoZs4NSPg8og/zNECKqT6eIO7H1pZb06PhB/Hf+qieoFJkMfu1DWAMJY6XgthDha5pLlSr/AZqkIS6xUAuM4W6KAskvm4XzmnP9S9i04TsE4s+XrpOS0lZHawXsTmShVo2W1skqfJucos2p+35n9yjvmfDGxgPJJmKDDTD9ngLjmNjarW1OdIvB99Os1pxnqg5ZgFmQvJkpO+dV8WxU6SU8FF8B/F0TWviv9g+a67Fm+2cZJXDdCkxJHk27vZjCJCVZcYFcQmCOHwRULTaq8PbEC3Pnk8bPuA9Cg7gQPrWoa5VQ1OQWXDT/ny5f4a8qNfBw5VnjByMMBsn3g//J test@openssh_test' -e SUDO_ACCESS=false -e PASSWORD_ACCESS=false -e USER_NAME=test -l traefik.enable=false solipsist01/openssh-server
+docker run -d --name=openssh_test --network=nasi -e PUID=1000 -e PGID=1000 -e TZ=europe/amsterdam -e PUBLIC_KEY='ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCsIBoZs4NSPg8og/zNECKqT6eIO7H1pZb06PhB/Hf+qieoFJkMfu1DWAMJY6XgthDha5pLlSr/AZqkIS6xUAuM4W6KAskvm4XzmnP9S9i04TsE4s+XrpOS0lZHawXsTmShVo2W1skqfJucos2p+35n9yjvmfDGxgPJJmKDDTD9ngLjmNjarW1OdIvB99Os1pxnqg5ZgFmQvJkpO+dV8WxU6SU8FF8B/F0TWviv9g+a67Fm+2cZJXDdCkxJHk27vZjCJCVZcYFcQmCOHwRULTaq8PbEC3Pnk8bPuA9Cg7gQPrWoa5VQ1OQWXDT/ny5f4a8qNfBw5VnjByMMBsn3g//J test@openssh_test' -e SUDO_ACCESS=false -e PASSWORD_ACCESS=false -e USER_NAME=test -l traefik.enable=false solipsist01/openssh-server
 echo "wachten op de initialisatie"
 countdown 10
 echo "rechten aanpassen van test private key"
